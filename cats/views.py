de24ceb3082a190d43192ec6f4027aab6377f203
Иван Лебедев <ivan_lbd@mail.ru>
2024-04-11 17:21:29 +0300

    sprint 14, lesson 4

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
+from .throttling import WorkingHoursRateThrottle
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .permissions import OwnerOrReadOnly, ReadOnly
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    # Вот он наш собственный класс пагинации с page_size=20
+    pagination_class = CatsPagination
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
